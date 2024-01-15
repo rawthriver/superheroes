a0e360431441395b7cb0ea2ad222e2af36f06065
rawthriver <rawthriver@yandex.ru>
2024-01-15 20:53:00 +0300

    search page action added

--- lib/pages/search_results_page.dart
+++ lib/pages/search_results_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:superheroes/pages/superhero_page.dart';
 import 'package:superheroes/resources/superheroes_colors.dart';
 import 'package:superheroes/widgets/superhero_card.dart';
 
@@ -7,15 +8,19 @@ class SearchResultsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Align(
+    VoidCallback openSuperHeroPage(name) {
+      return () => Navigator.of(context).push(MaterialPageRoute(builder: (context) => SuperheroPage(name: name)));
+    }
+
+    return Align(
       alignment: Alignment.topCenter,
       child: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 16),
+        padding: const EdgeInsets.symmetric(horizontal: 16),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            SizedBox(height: 114),
-            Text(
+            const SizedBox(height: 114),
+            const Text(
               'Search results',
               style: TextStyle(
                 fontSize: 20,
@@ -23,17 +28,19 @@ class SearchResultsPage extends StatelessWidget {
                 color: SuperheroesColors.text,
               ),
             ),
-            SizedBox(height: 20),
+            const SizedBox(height: 20),
             SuperheroCard(
               name: 'Batman',
               realName: 'Bruce Wayne',
               imageUrl: 'https://www.superherodb.com/pictures2/portraits/10/100/639.jpg',
+              action: openSuperHeroPage('Batman'),
             ),
-            SizedBox(height: 8),
+            const SizedBox(height: 8),
             SuperheroCard(
               name: 'Venom',
               realName: 'Eddie Brock',
               imageUrl: 'https://www.superherodb.com/pictures2/portraits/10/100/22.jpg',
+              action: openSuperHeroPage('Venom'),
             ),
           ],
         ),
